03f541f0f5e49699790b3c4874fcdb31d83b5e56
Aman Singh <amanpratap1437@gmail.com>
2025-01-23 15:34:16 +0530

    learn react core topic

---
+++ 01-react-essentials/src/components/TabButton.jsx
@@ -0,0 +1,12 @@
+export default function TabButton({children, onSelect, isSelected}){
+
+    function handleClick(){
+        console.log("Button Clicked");
+    }
+
+    return(
+        <li>
+            <button className={isSelected ? "active": ""} onClick={onSelect}>{children}</button>
+        </li>
+    );
+}
