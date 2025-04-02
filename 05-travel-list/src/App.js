c7d05c9ce6aeaeb1f63880c06b518cf5b5914e19
sachindbt <sachin.kd@echidnainc.com>
2025-04-02 13:22:54 +0530

    Thinking in React-state management

---
+++ 05-travel-list/src/App.js
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import Logo from "./components/Logo";
+import { Form } from "./components/Form";
+import PackingList from "./components/PackingList";
+import { Stats } from "./components/Stats";
+
+
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItem(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleTab(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleClearList(){
+    setItems([]);
+  }
+
+  return (
+    <div className="app">
+      <Logo />
+      <Form onAddItem={handleAddItem} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleTab}
+        onClearList={handleClearList}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
+
+
+
+
+
+
+
+
+
+
---
+++ 05-travel-list/src/components/Form.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+export function Form({ onAddItem }) {
+  const [quantity, setQuantity] = useState(1);
+  const [description, setDescription] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { quantity, description, packed: false, id: Date.now() };
+    console.log(newItem);
+
+    onAddItem(newItem);
+
+    setQuantity(1);
+    setDescription("");
+  }
+
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What you need for your trip ?</h3>
+      <select
+        value={quantity}
+        onChange={(event) => setQuantity(event.target.value)}
+      >
+        {Array.from({ length: 10 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Items..."
+        value={description}
+        onChange={(event) => setDescription(event.target.value)}
+        required
+      />
+      <button>Add</button>
+    </form>
+  );
+}
