fa3cfcccb7554c444e7d18775366dfe5c89b3caf
Aman Singh <amanpratap1437@gmail.com>
2025-04-15 14:52:18 +0530

    learn about useReducer

---
+++ 08-react-quiz/src/components/Question.js
@@ -0,0 +1,9 @@
+export default function Question({question}){
+    console.log(question)
+    return <div>
+        <h4>{question.question}</h4>
+        <div className="options">
+            {question.options.map((option)=><button className="btn btn-option" key={option}>{option}</button>)}
+        </div>
+    </div>
+}
